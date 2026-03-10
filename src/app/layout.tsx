3124a246746abc858571d6718c6d0f06cc4b1d24
RyanLeungTsu <ryanleungtsu@gmail.com>
2026-03-09 17:19:45 -0700

    installed and added vercel analytics in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// For tracking analytics with vercel
+import { Analytics } from "@vercel/analytics/next"
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
