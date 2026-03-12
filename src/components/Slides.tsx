e4fc846fa9ad56eb5bbf46cd34a3153c5297b873
RyanLeungTsu <ryanleungtsu@gmail.com>
2026-03-11 18:03:21 -0700

    fixed scaling issue in slides

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -242,22 +242,22 @@ const Slides: React.FC<SlidesProps> = ({ cell, close }) => {
                 <Rnd
                   key={el.id}
                   size={{
-                    width: (el.width ?? 200) * scale,
-                    height: (el.height ?? 100) * scale,
+                    width: el.width ?? 200,
+                    height: el.height ?? 100,
                   }}
-                  position={{ x: el.x * scale, y: el.y * scale }}
+                  position={{ x: el.x, y: el.y }}
                   bounds="parent"
                   scale={scale}
                   dragHandleClassName="drag-handle"
                   onDragStop={(_, d) =>
-                    updateElement(el.id, { x: d.x / scale, y: d.y / scale })
+                    updateElement(el.id, { x: d.x, y: d.y })
                   }
                   onResizeStop={(_, __, ref, ___, position) =>
                     updateElement(el.id, {
-                      width: ref.offsetWidth / scale,
-                      height: ref.offsetHeight / scale,
-                      x: position.x / scale,
-                      y: position.y / scale,
+                      width: ref.offsetWidth,
+                      height: ref.offsetHeight,
+                      x: position.x,
+                      y: position.y,
                     })
                   }
                   enableResizing={{
@@ -300,12 +300,65 @@ const Slides: React.FC<SlidesProps> = ({ cell, close }) => {
                           }
                         />
                         <div className="absolute bottom-1 left-1 flex gap-1">
-                          <button onClick={() => updateElement(el.id, { fontSize: (el.fontSize ?? DefaultFontSize) + 2 })} className="bg-gray-300 p-1 rounded text-xs" title="Increase Font">A+</button>
-                          <button onClick={() => updateElement(el.id, { fontSize: Math.max((el.fontSize ?? DefaultFontSize) - 2, 8) })} className="bg-gray-300 p-1 rounded text-xs" title="Decrease Font">A-</button>
-                          <button onClick={() => updateElement(el.id, { textAlign: "left" })} className="bg-gray-300 p-1 rounded text-xs" title="Align Left">⬅</button>
-                          <button onClick={() => updateElement(el.id, { textAlign: "center" })} className="bg-gray-300 p-1 rounded text-xs" title="Align Center">⬌</button>
-                          <button onClick={() => updateElement(el.id, { textAlign: "right" })} className="bg-gray-300 p-1 rounded text-xs" title="Align Right">➡</button>
-                          <button onClick={() => removeElement(el.id)} className="bg-red-500 text-white p-1 rounded text-xs hover:bg-red-600" title="Delete Text Box">✕</button>
+                          <button
+                            onClick={() =>
+                              updateElement(el.id, {
+                                fontSize: (el.fontSize ?? DefaultFontSize) + 2,
+                              })
+                            }
+                            className="bg-gray-300 p-1 rounded text-xs"
+                            title="Increase Font"
+                          >
+                            A+
+                          </button>
+                          <button
+                            onClick={() =>
+                              updateElement(el.id, {
+                                fontSize: Math.max(
+                                  (el.fontSize ?? DefaultFontSize) - 2,
+                                  8,
+                                ),
+                              })
+                            }
+                            className="bg-gray-300 p-1 rounded text-xs"
+                            title="Decrease Font"
+                          >
+                            A-
+                          </button>
+                          <button
+                            onClick={() =>
+                              updateElement(el.id, { textAlign: "left" })
+                            }
+                            className="bg-gray-300 p-1 rounded text-xs"
+                            title="Align Left"
+                          >
+                            ⬅
+                          </button>
+                          <button
+                            onClick={() =>
+                              updateElement(el.id, { textAlign: "center" })
+                            }
+                            className="bg-gray-300 p-1 rounded text-xs"
+                            title="Align Center"
+                          >
+                            ⬌
+                          </button>
+                          <button
+                            onClick={() =>
+                              updateElement(el.id, { textAlign: "right" })
+                            }
+                            className="bg-gray-300 p-1 rounded text-xs"
+                            title="Align Right"
+                          >
+                            ➡
+                          </button>
+                          <button
+                            onClick={() => removeElement(el.id)}
+                            className="bg-red-500 text-white p-1 rounded text-xs hover:bg-red-600"
+                            title="Delete Text Box"
+                          >
+                            ✕
+                          </button>
                         </div>
                       </>
                     )}
@@ -324,7 +377,12 @@ const Slides: React.FC<SlidesProps> = ({ cell, close }) => {
                             color: "var(--text)",
                           }}
                         />
-                        <button onClick={() => removeElement(el.id)} className="absolute top-1 right-1 bg-red-500 text-white rounded-full w-6 h-6 hover:bg-red-600">✕</button>
+                        <button
+                          onClick={() => removeElement(el.id)}
+                          className="absolute top-1 right-1 bg-red-500 text-white rounded-full w-6 h-6 hover:bg-red-600"
+                        >
+                          ✕
+                        </button>
                       </div>
                     )}
                   </div>
@@ -341,11 +399,34 @@ const Slides: React.FC<SlidesProps> = ({ cell, close }) => {
                   }}
                 >
                   {el.kind === "text" && (
-                    <div style={{ width: "100%", height: "100%", fontSize: el.fontSize ?? DefaultFontSize, color: "var(--text)", textAlign: el.textAlign ?? "left", whiteSpace: "pre-wrap", padding: 4, boxSizing: "border-box" }}>
+                    <div
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        fontSize: el.fontSize ?? DefaultFontSize,
+                        color: "var(--text)",
+                        textAlign: el.textAlign ?? "left",
+                        whiteSpace: "pre-wrap",
+                        padding: 4,
+                        boxSizing: "border-box",
+                      }}
+                    >
                       {el.content}
                     </div>
                   )}
-                  {el.kind === "image" && <MediaDisplay element={el} style={{ width: "100%", height: "100%", objectFit: "contain", maxWidth: "none", maxHeight: "none", display: "block" }} />}
+                  {el.kind === "image" && (
+                    <MediaDisplay
+                      element={el}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "contain",
+                        maxWidth: "none",
+                        maxHeight: "none",
+                        display: "block",
+                      }}
+                    />
+                  )}
                   {el.kind === "audio" && <MediaDisplay element={el} />}
                   {el.kind === "video" && <MediaDisplay element={el} />}
                 </div>
@@ -357,44 +438,127 @@ const Slides: React.FC<SlidesProps> = ({ cell, close }) => {
         {/* Control button ui for slides */}
         <div className="flex flex-wrap justify-center gap-2 mb-2">
           {/* Prev Slide Button */}
-          <button onClick={() => setCurrentSlideIndex(Math.max(currentSlideIndex - 1, 0))} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-blue-700 to-teal-200 text-blue-500 hover:text-white focus:outline-none focus:ring-0">
-            <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">Prev</span>
+          <button
+            onClick={() =>
+              setCurrentSlideIndex(Math.max(currentSlideIndex - 1, 0))
+            }
+            className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-blue-700 to-teal-200 text-blue-500 hover:text-white focus:outline-none focus:ring-0"
+          >
+            <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+              Prev
+            </span>
           </button>
           {/* Toggle for editing and non-editing for slides */}
-          <button onClick={() => setEditing(!editing)} className={`relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold group ${editing ? "bg-gradient-to-br from-yellow-400 to-red-400 text-orange-600" : "bg-gradient-to-br from-orange-500 to-purple-500 text-orange-400"} focus:outline-none focus:ring-0 hover:text-white`}>
-            <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">{editing ? "Save" : "Edit"}</span>
+          <button
+            onClick={() => {
+              setEditing(!editing);
+              if (editing) savedSlide(false);
+            }}
+            className={`relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold group ${editing ? "bg-gradient-to-br from-yellow-400 to-red-400 text-orange-600" : "bg-gradient-to-br from-orange-500 to-purple-500 text-orange-400"} focus:outline-none focus:ring-0 hover:text-white`}
+          >
+            <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+              {editing ? "Save" : "Edit"}
+            </span>
           </button>
           {/* Next slide button */}
-          <button onClick={() => { if (currentSlideIndex < slides.length - 1) setCurrentSlideIndex(currentSlideIndex + 1); }} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-blue-700 to-teal-200 text-blue-500 hover:text-white focus:outline-none focus:ring-0">
-            <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">Next</span>
+          <button
+            onClick={() => {
+              if (currentSlideIndex < slides.length - 1)
+                setCurrentSlideIndex(currentSlideIndex + 1);
+            }}
+            className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-blue-700 to-teal-200 text-blue-500 hover:text-white focus:outline-none focus:ring-0"
+          >
+            <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+              Next
+            </span>
           </button>
           {/* Button for adding a slide */}
           {editing && (
-            <button onClick={() => { const newSlide: Slide = { elements: [{ id: crypto.randomUUID(), kind: "text", content: "", x: 20, y: 20, width: 200, height: 100, fontSize: DefaultFontSize }] }; const updatedSlides = [...slides]; updatedSlides.splice(currentSlideIndex + 1, 0, newSlide); setSlides(updatedSlides); setCurrentSlideIndex(currentSlideIndex + 1); savedSlide(true); }} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold group bg-gradient-to-br from-teal-300 to-lime-400 text-teal-500 hover:text-white focus:outline-none focus:ring-0">
-              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">Add Slide</span>
+            <button
+              onClick={() => {
+                const newSlide: Slide = {
+                  elements: [
+                    {
+                      id: crypto.randomUUID(),
+                      kind: "text",
+                      content: "",
+                      x: 20,
+                      y: 20,
+                      width: 200,
+                      height: 100,
+                      fontSize: DefaultFontSize,
+                    },
+                  ],
+                };
+                const updatedSlides = [...slides];
+                updatedSlides.splice(currentSlideIndex + 1, 0, newSlide);
+                setSlides(updatedSlides);
+                setCurrentSlideIndex(currentSlideIndex + 1);
+                savedSlide(true);
+              }}
+              className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold group bg-gradient-to-br from-teal-300 to-lime-400 text-teal-500 hover:text-white focus:outline-none focus:ring-0"
+            >
+              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+                Add Slide
+              </span>
             </button>
           )}
           {/* Button for deleting a slide */}
           {editing && slides.length > 1 && (
-            <button onClick={deleteCurrentSlide} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-red-600 to-orange-300 text-red-700 hover:text-white focus:outline-none focus:ring-0">
-              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">Delete Slide</span>
+            <button
+              onClick={deleteCurrentSlide}
+              className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-red-600 to-orange-300 text-red-700 hover:text-white focus:outline-none focus:ring-0"
+            >
+              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+                Delete Slide
+              </span>
             </button>
           )}
           {/* Add media button for slides */}
           {editing && (
-            <button onClick={() => { setMediaTarget(currentSlideIndex); setTimeout(() => mediaInputRef.current?.click(), 0); }} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-purple-700 to-pink-400 text-purple-500 hover:text-white focus:outline-none focus:ring-0">
-              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">Add Media</span>
+            <button
+              onClick={() => {
+                setMediaTarget(currentSlideIndex);
+                setTimeout(() => mediaInputRef.current?.click(), 0);
+              }}
+              className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-purple-700 to-pink-400 text-purple-500 hover:text-white focus:outline-none focus:ring-0"
+            >
+              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+                Add Media
+              </span>
             </button>
           )}
           {/* Adding more text boxes than default */}
           {editing && (
-            <button onClick={() => { const updated = [...slides]; updated[currentSlideIndex].elements.push({ id: crypto.randomUUID(), kind: "text", content: "", x: 100, y: 100, width: 200, height: 100, fontSize: DefaultFontSize }); setSlides(updated); savedSlide(true); }} className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-blue-700 to-teal-200 text-teal-500 hover:text-white focus:outline-none focus:ring-0">
-              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">Add Text</span>
+            <button
+              onClick={() => {
+                const updated = [...slides];
+                updated[currentSlideIndex].elements.push({
+                  id: crypto.randomUUID(),
+                  kind: "text",
+                  content: "",
+                  x: 100,
+                  y: 100,
+                  width: 200,
+                  height: 100,
+                  fontSize: DefaultFontSize,
+                });
+                setSlides(updated);
+                savedSlide(true);
+              }}
+              className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-bold text-heading group bg-gradient-to-br from-blue-700 to-teal-200 text-teal-500 hover:text-white focus:outline-none focus:ring-0"
+            >
+              <span className="relative px-4 py-2.5 transition-all ease-in duration-350 bg-white group-hover:bg-transparent">
+                Add Text
+              </span>
             </button>
           )}
         </div>
         {/* Upload Media */}
-        <MediaUploader ref={mediaInputRef} onAdd={(type, mediaId) => handleMediaAdded(type, mediaId)} />
+        <MediaUploader
+          ref={mediaInputRef}
+          onAdd={(type, mediaId) => handleMediaAdded(type, mediaId)}
+        />
       </div>
     </div>
   );
